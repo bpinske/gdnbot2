93b1c710b46a201888ae6ad51a473e050f61df6b
Matthew Miller <matthew@millerti.me>
2019-03-16 11:32:30 -0700

    Restart bot with Commando

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,25 @@
 // Load files from the .env file
 require('dotenv').config();
 
-const { Client } = require('discord.js');
+const { CommandoClient } = require('discord.js-commando');
+const path = require('path');
 
-const bot = new Client();
+const bot = new CommandoClient({
+  prefix: '!',
+  owner: '148474055949942787',
+  invite: 'https://discord.gg/vH8uVUE'
+});
+
+bot.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ['auth', 'Auth Services'],
+    ['gdn', 'GDN Services']
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands()
+  // Automatically load commands that exist in the commands/ directory
+  .registerCommandsIn(path.join(__dirname, 'commands'));
 
 bot.once('ready', () => {
   console.log('   __________  _   ______        __');
@@ -13,10 +29,14 @@ bot.once('ready', () => {
   console.log('\\____/_____/_/ |_/_____/\\____/\\__/');
   console.log(`Logged in as ${bot.user.tag}`);
   console.log('---:getin:---');
+
+  bot.user.setActivity('in the forge');
 });
 
+bot.on('error', console.error);
+
 bot.on('message', (message) => {
-  console.log(message.content);
+  // console.log(message.content);
 });
 
 bot.login(process.env.DISCORD_BOT_TOKEN);
