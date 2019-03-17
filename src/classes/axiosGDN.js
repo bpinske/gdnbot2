9d47ad17b8bc566dd7a6b873e1ab76ba157630e0
Matthew Miller <matthew@millerti.me>
2019-03-16 21:13:26 -0700

    Create an instance of Axios for GDN API

---
+++ src/classes/axiosGDN.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+/**
+ * An instance of Axios configured to speak with the GDN APIs over Docker's internal network
+ */
+const axiosGDN = axios.create({
+  baseUrl: 'http://gdn/',
+  headers: {
+    'Authorization': `Token ${process.env.GDN_API_TOKEN}`,
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+});
+
+module.exports = axiosGDN;
