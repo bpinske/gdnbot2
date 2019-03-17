e9a427380124b3772b759ff29e851f7c67a51a8d
Matthew Miller <matthew@millerti.me>
2019-03-16 21:23:20 -0700

    Fix misconfiguration in GDN client

--- src/classes/axiosGDN.js
+++ src/classes/axiosGDN.js
@@ -4,7 +4,7 @@ const axios = require('axios');
  * An instance of Axios configured to speak with the GDN APIs over Docker's internal network
  */
 const axiosGDN = axios.create({
-  baseUrl: 'http://gdn/',
+  baseURL: 'http://gdn/',
   headers: {
     'Authorization': `Token ${process.env.GDN_API_TOKEN}`,
     'Content-Type': 'application/json',
