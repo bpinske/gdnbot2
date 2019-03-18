9f6e59a2c1e9903d1d4a2927877908e4cdefdf7b
Matthew Miller <matthew@millerti.me>
2019-03-17 21:30:58 -0700

    Add logging and timeout to guildMemberAdd

--- src/helpers/auth/guildMemberAdd.js
+++ src/helpers/auth/guildMemberAdd.js
@@ -1,6 +1,6 @@
 const { SnowflakeUtil } = require('discord.js');
 
-const logger = require('../logger');
+const { logger } = require('../');
 const startAuthCheck = require('./startAuthCheck');
 
 /**
@@ -14,7 +14,12 @@ const guildMemberAdd = (member) => {
   // Generate a logging tag with the snowflake
   const tag = logger.getLogTag(eventId);
 
-  startAuthCheck({ tag, guild, member, isAuthMe: false });
+  logger.info(tag, `Member ${member.name} joined guild ${guild.name}`);
+
+  // Wait a second before proceeding with auto-auth
+  setTimeout(() => {
+    startAuthCheck({ tag, guild, member, isAuthMe: false });
+  }, 1000);
 };
 
 module.exports = guildMemberAdd;
