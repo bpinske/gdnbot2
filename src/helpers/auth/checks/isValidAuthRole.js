bc3f5697d466c026ac7d7b7ee0b975610dc5bf13
Matthew Miller <matthew@millerti.me>
2019-03-22 22:34:52 -0700

    Add isValidAuthRole check

---
+++ src/helpers/auth/checks/isValidAuthRole.js
@@ -0,0 +1,42 @@
+const { oneLine } = require('common-tags');
+
+const {
+  logger
+} = require('../../');
+
+const invalidRoleReason = oneLine`
+  \`!authme\` doesn't appear to be set up properly here. Please contact a guild admin and ask them
+  to "re-activate auth with an updated role ID".
+`;
+
+/**
+ * Validate the server's authme role ID to make sure it references a valid role
+ *
+ * @param {object} tag - The output from a call to logger.getLogTag()
+ * @param {Member} guild - The guild whose roles will be searched
+ * @param {string} roleId - The auth role ID provided when auth was activated
+ * @returns {object} - { isValid, reason?, validatedRole }
+ */
+const isValidAuthRole = async ({ tag, guild, roleId }) => {
+  logger.info(tag, `Validating auth role ID "${roleId}"`);
+
+  const validatedRole = await guild.roles.fetch(roleId);
+
+  if (!validatedRole) {
+    logger.info(tag, `Could not find a role with that ID in guild, returning false`);
+    return {
+      isValid: false,
+      reason: invalidRoleReason
+    };
+  }
+
+  logger.info(tag, `Found valid role: "${validatedRole.name}"`);
+
+  return {
+    isValid: true,
+    // Provide the actual role since we already did the work of looking it up
+    validatedRole
+  };
+};
+
+module.exports = isValidAuthRole;
