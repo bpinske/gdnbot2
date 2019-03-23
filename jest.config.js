3dbdcd3be4e26a801d91d144971a30d23f01dcd5
Matthew Miller <matthew@millerti.me>
2019-03-22 23:51:10 -0700

    Add unit testing with Jest

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: [
+    "src/**/*.{js,ts}",
+  ],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  setupFilesAfterEnv: [
+    "<rootDir>/src/setupTests.js"
+  ],
+
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: [
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+};
---
+++ src/setupTests.js
@@ -0,0 +1,1 @@
+// Put Jest setup code here
