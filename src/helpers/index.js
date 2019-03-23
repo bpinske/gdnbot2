9cdf34514e20809b8950ccde4b5b8f8b78a001b7
Matthew Miller <matthew@millerti.me>
2019-03-22 17:51:02 -0700

    Update exports in helpers index.js

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,10 +1,11 @@
-const axiosGDN = require('./axiosGDN');
+const { axiosGDN, GDN_URLS } = require('./axiosGDN');
 const cleanupMessages = require('./cleanupMessages');
 const GDNEmbed = require('./GDNEmbed');
 const logger = require('./logger');
 
 module.exports = {
   axiosGDN,
+  GDN_URLS,
   cleanupMessages,
   GDNEmbed,
   logger
