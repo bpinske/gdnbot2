e270936adda29dd5da596b81481e60cb28e7605e
Matthew Miller <matthew@millerti.me>
2019-03-22 17:52:02 -0700

    Add new auth check helpers

---
+++ src/helpers/auth/checks/canMemberAuth.js
@@ -0,0 +1,76 @@
+const { oneLine } = require('common-tags');
+
+const {
+  logger,
+  axiosGDN,
+  GDN_URLS
+} = require('../../');
+
+const catchErrorMessage = oneLine`
+  An error occurred while attempting to verify that you can proceed with auth. The bot
+  owner has been notified. Thank you for your patience while they get this fixed!
+`;
+
+/**
+ * Check to see if a member has authed before in Goon Discord Network
+ *
+ * @param {object} tag - The output from a call to logger.getLogTag()
+ * @param {Member} member - The member to verify enrollment in GDN
+ * @returns {object} - { canAuth, reason? }
+ */
+const canMemberAuth = async ({ tag, member }) => {
+  let dataGDN;
+
+  logger.info(tag, `Checking if member ${member.name} has authed in GDN`);
+  try {
+    const resp = await axiosGDN.get(`${GDN_URLS.MEMBERS}/${member.id}`);
+    dataGDN = resp.data;
+    logger.info(tag, 'Member has authed in GDN');
+  } catch (err) {
+    const { response } = err;
+
+    if (response && response.status === 404) {
+      logger.info(tag, 'Member has not authed before, OK to auth');
+      return {
+        canAuth: true
+      };
+    } else {
+      logger.error({ ...tag, err }, 'Error checking if member has authed');
+      return {
+        canAuth: false,
+        reason: catchErrorMessage
+      };
+    }
+  }
+
+  logger.info(tag, `Checking if member ${member.name} is blacklisted`);
+  try {
+    logger.info(tag, 'Requesting internal GDN member profile by SA ID');
+    const { data } = await axiosGDN.get(`${GDN_URLS.SA}/${dataGDN.sa_id}`);
+
+    if (data.blacklisted) {
+      logger.warn(tag, 'Member has authed before but is BLACKLISTED, ignoring');
+      return {
+        canAuth: false,
+        reason: oneLine`
+          You are blacklisted from the Goon Discord Network. You may appeal this decision here:
+          https://discord.gg/vH8uVUE
+        `
+      };
+    }
+  } catch (err) {
+    logger.error({ ...tag, err }, 'Error checking if member is blacklisted');
+    return {
+      canAuth: false,
+      reason: catchErrorMessage
+    };
+  }
+
+  // User has authed before and isn't blacklisted, so allow them to auth
+  logger.info(tag, 'Member is not blacklisted, OK to auth');
+  return {
+    canAuth: true
+  };
+};
+
+module.exports = canMemberAuth;
---
+++ src/helpers/auth/checks/hasGuildEnrolled.js
@@ -0,0 +1,55 @@
+const { oneLine } = require('common-tags');
+
+const {
+  logger,
+  axiosGDN,
+  GDN_URLS
+} = require('../../');
+
+/**
+ * Check to see if a guild is enrolled in Goon Discord Network
+ *
+ * @param {object} tag - The output from a call to logger.getLogTag()
+ * @param {Guild} guild - The guild to verify enrollment in GDN
+ * @returns {object} - { enrolled, reason, roleId, channelId }
+ */
+const hasGuildEnrolled = async ({ tag, guild }) => {
+  logger.info(tag, `Checking if guild ${guild.name} has enrolled in GDN`);
+
+  try {
+    // Not erroring out here means the server is in GDN
+    const { data } = await axiosGDN.get(`${GDN_URLS.GUILDS}/${guild.id}`);
+
+    logger.info(tag, 'Server is enrolled in GDN');
+
+    return {
+      enrolled: true,
+      roleId: data.validated_role_id,
+      channelId: data.logging_channel_id
+    };
+  } catch (err) {
+    const { response } = err;
+
+    if (response && response.status === 404) {
+      logger.info(tag, '...but no server info was found, returning false');
+      return {
+        enrolled: false,
+        reason: oneLine`
+          This server is not enrolled in the Goon Discord Network. Please have an
+          admin enroll the server and then activate auth.
+        `
+      };
+    } else {
+      logger.error({ ...tag, err }, 'Error checking for server info');
+      return {
+        enrolled: false,
+        reason: oneLine`
+          An error occurred while attempting to verify guild enrollment in GDN. The bot owner has
+          been notified. Thank you for your patience while they get this fixed!
+        `
+      };
+    }
+  }
+};
+
+module.exports = hasGuildEnrolled;
