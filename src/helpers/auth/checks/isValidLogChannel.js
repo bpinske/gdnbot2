a27db0cfd5ef85b76e46753465640762f55726be
Matthew Miller <matthew@millerti.me>
2019-04-01 23:51:50 -0700

    Tweak logger messages when validating log channel

--- src/helpers/auth/checks/isValidLogChannel.js
+++ src/helpers/auth/checks/isValidLogChannel.js
@@ -17,11 +17,11 @@ const isValidLogChannel = async ({ tag, guild, channelId }) => {
     // Convert to string in case we get a number
     const id = String(channelId);
 
-    logger.info(tag, `Validating logging channel ID: ${id}`);
+    logger.info(tag, `Validating logging channel ID: '${id}'`);
     validatedChannel = await guild.channels.get(id);
 
     if (validatedChannel) {
-      logger.info(tag, `Found valid channel: ${validatedChannel.name}`);
+      logger.info(tag, `Found valid channel: '#${validatedChannel.name}'`);
     } else {
       logger.info(tag, 'No channel found by that ID');
     }
