ff7f9f34aef193eaeba807accfcedf1fd0a61fc1
Matthew Miller <matthew@millerti.me>
2019-04-06 15:37:44 -0700

    Add more comments to getSAID

--- src/helpers/auth/actions/getSAID.js
+++ src/helpers/auth/actions/getSAID.js
@@ -36,6 +36,9 @@ const reasonCatchError = oneLine`
 /**
  * Scrape the user's SA profile for their SA ID
  *
+ * This will get called _after_ the hash has been successfully verified, which means we're
+ * assuming the username is valid.
+ *
  * @param {object} tag - The output from a call to logger.getLogTag()
  * @param {string} username - The member's SA Username
  * @return {object} - { id }
@@ -53,6 +56,13 @@ async function getSAID ({ tag, username }) {
     // Try to grab the ID
     const id = $('input[name="userid"]').val();
 
+    /**
+     * Note to self: If no ID is returned, then perhaps:
+     *
+     * 1) the cookies above are invalid (maybe the account is banned)
+     * 2) Something about the profile page HTML changed
+     * 3) Maybe the username WAS mis-spelled
+     */
     if (!id) {
       logger.error(tag, `No user ID was found on ${url}`);
       return {
