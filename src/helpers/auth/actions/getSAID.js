b5903b642e2e53fc8c64869c744686b97553bc9e
Matthew Miller <matthew@millerti.me>
2019-04-06 15:28:27 -0700

    Build out getSAID

--- src/helpers/auth/actions/getSAID.js
+++ src/helpers/auth/actions/getSAID.js
@@ -1,3 +1,38 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { oneLine } = require('common-tags');
+
+const logger = require('../../logger');
+
+const PROFILE_URL = 'http://forums.somethingawful.com/member.php?action=getinfo&username=';
+
+const COOKIES = {
+  sessionid: '87654063',
+  sessionhash: '6cd7e0953b0c6cd47ac6860ce6271d87',
+  bbuserid: '186135',
+  bbpassword: 'e6cf0cd6bc8218c6a361755d76deae38'
+};
+
+// Format the above cookies into a value suitable for use as the Cookie header
+const Cookie = Object.entries(COOKIES).map(([key, val]) => `${key}=${val}`).join('; ');
+
+// Prepare an Axios client with SA user cookies
+const saScraper = axios.create({
+  headers: {
+    Cookie
+  }
+});
+
+const reasonNoIDFound = oneLine`
+  I could not find an ID on the SA profile page for the username you provided. The bot owner has
+  been notified. Thank you for your patience while they get this fixed!
+`;
+
+const reasonCatchError = oneLine`
+  A system error occurred while attempting to retrieve your account ID. The bot owner has been
+  notified. Thank you for your patience while they get this fixed!
+`;
+
 /**
  * Scrape the user's SA profile for their SA ID
  *
@@ -5,8 +40,38 @@
  * @param {string} username - The member's SA Username
  * @return {object} - { id }
  */
-const getSAID = ({ tag, username }) => {
+async function getSAID ({ tag, username }) {
+  logger.info(tag, `Finding user ID for SA user ${username}`);
 
-};
+  const url = `${PROFILE_URL}${username}`;
+
+  try {
+    // Request HTML
+    const resp = await saScraper.get(url);
+    // Prepare to parse it
+    const $ = cheerio.load(resp.data);
+    // Try to grab the ID
+    const id = $('input[name="userid"]').val();
+
+    if (!id) {
+      logger.error(tag, `No user ID was found on ${url}`);
+      return {
+        id: null,
+        reason: reasonNoIDFound
+      };
+    }
+
+    logger.info(tag, `Found user ID: ${id}`);
+    return {
+      id
+    };
+  } catch (err) {
+    logger.error({ ...tag, err }, 'System error retrieving SA user ID');
+    return {
+      id: null,
+      reason: reasonCatchError
+    };
+  }
+}
 
 module.exports = getSAID;
