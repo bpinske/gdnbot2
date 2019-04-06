5c51e3eb41a0d4d3a5d747839680b4f83537da27
Matthew Miller <matthew@millerti.me>
2019-04-06 12:43:19 -0700

    Add new getSAID auth action

---
+++ src/helpers/auth/actions/getSAID.js
@@ -0,0 +1,12 @@
+/**
+ * Scrape the user's SA profile for their SA ID
+ *
+ * @param {object} tag - The output from a call to logger.getLogTag()
+ * @param {string} username - The member's SA Username
+ * @return {object} - { id }
+ */
+const getSAID = ({ tag, username }) => {
+
+};
+
+module.exports = getSAID;
