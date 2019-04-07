369f914b7cbee821933d2bd90635153736321ba9
Matthew Miller <matthew@millerti.me>
2019-04-06 21:44:06 -0700

    Add comments to addUserToDB

--- src/helpers/auth/actions/addUserToDB.js
+++ src/helpers/auth/actions/addUserToDB.js
@@ -1,16 +1,24 @@
 const logger = require('../../logger');
 const { axiosGDN, GDN_URLS } = require('../../axiosGDN');
 
+/**
+ * Insert the authed user into the GDN database
+ *
+ * @param {object} tag - The output from a call to logger.getLogTag()
+ * @param {Member} member - The Discord Member that completed authentication
+ * @param {string} saID - The Member's SomethingAwful ID
+ * @param {string} saUsername - The Member's SomethingAwful username
+ */
 const addUserToDB = async ({ tag, member, saID, saUsername }) => {
-  logger.info(tag, 'Inserting member into database');
-
-  const payload = {
-    discord_id: member.id,
-    sa_id: saID,
-    sa_username: saUsername.substr(0, 19)
-  };
-
   try {
+    const payload = {
+      discord_id: member.id,
+      sa_id: saID,
+      sa_username: saUsername.substr(0, 19)
+    };
+
+    logger.info(tag, `Inserting member into database: ${JSON.stringify(payload)}`);
+
     await axiosGDN.post(GDN_URLS.MEMBERS, payload);
     logger.info(tag, 'Successfully inserted member');
   } catch (err) {
