d76513513db8feb57a8a14410144d52639ad52c3
Matthew Miller <matthew@millerti.me>
2019-04-11 17:54:45 -0700

    Add more test cases for authme

--- tests/authme.spec.js
+++ tests/authme.spec.js
@@ -1,6 +1,8 @@
 const moxios = require('moxios');
 
 const AuthmeCommand = require('../src/commands/auth/authme');
+
+const logger = require('../src/helpers/logger');
 const { axiosGDN, GDN_URLS } = require('../src/helpers/axiosGDN');
 const { axiosGoonAuth, GOON_AUTH_URLS } = require('../src/helpers/axiosGoonAuth');
 const { SA_URLS } = require('../src/helpers/axiosSA');
@@ -72,10 +74,14 @@ beforeEach(() => {
     roles: {
       get () { return _guildRoles; },
       set (newRoles) {},
-      fetch: jest.fn().mockResolvedValue(_guildRoles[0])
+      fetch: jest.fn().mockImplementation(
+        (_id) => Promise.resolve(_id === roleID ? _guildRoles[0] : null)
+      )
     },
     channels: {
-      get: jest.fn().mockResolvedValue(_guildChannels[0])
+      get: jest.fn().mockImplementation(
+        (_id) => Promise.resolve(_id === channelID ? _guildChannels[0] : null)
+      )
     }
   };
 
@@ -141,10 +147,10 @@ test('adds role to user that has never authed before', async () => {
     status: 200
   });
 
-  // User enters "!authme saUsername"
   await authme.run(message, { username: saUsername });
 
   expect(member.edit).toHaveBeenCalledWith({ roles: [authRole], reason: 'GDN: Successful Auth' });
+  expect(logChannel.send).toHaveBeenCalledWith(`${member.user.tag} (SA: ${saUsername}) successfully authed`);
 });
 
 test('messages channel !authme was called in when Guild is not enrolled', async () => {
@@ -153,7 +159,6 @@ test('messages channel !authme was called in when Guild is not enrolled', async 
     status: 404
   });
 
-  // User enters "!authme saUsername"
   await authme.run(message, { username: saUsername });
 
   expect(message.say).toHaveBeenCalledWith('This server is not enrolled in the Goon Discord Network. Please have an admin enroll the server and then activate auth.');
@@ -165,7 +170,6 @@ test('messages channel !authme was called in when error occurs while checking Gu
     status: 500
   });
 
-  // User enters "!authme saUsername"
   await authme.run(message, { username: saUsername });
 
   expect(message.say).toHaveBeenCalledWith('A system error occurred while attempting to verify guild enrollment in GDN. The bot owner has been notified. Thank you for your patience while they get this fixed!');
@@ -181,7 +185,7 @@ test('skips hash check for user that has authed before and is not blacklisted', 
     }
   });
 
-  // Member has never authed before
+  // Member has authed before
   moxios.stubRequest(GDN_MEMBER, {
     status: 200,
     response: {
@@ -189,7 +193,7 @@ test('skips hash check for user that has authed before and is not blacklisted', 
     }
   });
 
-  // GoonAuth generates hash for user
+  // SA ID is not blacklisted
   moxios.stubRequest(GDN_SA, {
     status: 200,
     response: {
@@ -197,8 +201,132 @@ test('skips hash check for user that has authed before and is not blacklisted', 
     }
   });
 
-  // User enters "!authme saUsername"
   await authme.run(message, { username: saUsername });
 
   expect(member.edit).toHaveBeenCalledWith({ roles: [authRole], reason: 'GDN: Successful Auth' });
+  expect(logChannel.send).toHaveBeenCalledWith(`${member.user.tag} (SA: ${saUsername}) successfully authed`);
+});
+
+test('messages channel !authme was called in with blacklist rejection when user is blacklisted', async () => {
+  // Guild is enrolled in GDN
+  moxios.stubRequest(GDN_GUILD, {
+    status: 200,
+    response: {
+      validated_role_id: roleID,
+      logging_channel_id: channelID
+    }
+  });
+
+  // Member has authed before
+  moxios.stubRequest(GDN_MEMBER, {
+    status: 200,
+    response: {
+      sa_id: saID
+    }
+  });
+
+  // SA ID is blacklisted
+  moxios.stubRequest(GDN_SA, {
+    status: 200,
+    response: {
+      blacklisted: true
+    }
+  });
+
+  await authme.run(message, { username: saUsername });
+
+  expect(message.say).toHaveBeenCalledWith('You are blacklisted from the Goon Discord Network. You may appeal this decision here: https://discord.gg/vH8uVUE');
+});
+
+test('messages channel !authme was called in with invalid role reason if role registered by admin is no good', async () => {
+  // Guild is enrolled in GDN but has bad Role ID
+  moxios.stubRequest(GDN_GUILD, {
+    status: 200,
+    response: {
+      validated_role_id: 'badRoleID',
+      logging_channel_id: channelID
+    }
+  });
+
+  // Member has authed before
+  moxios.stubRequest(GDN_MEMBER, {
+    status: 200,
+    response: {
+      sa_id: saID
+    }
+  });
+
+  // SA ID is not blacklisted
+  moxios.stubRequest(GDN_SA, {
+    status: 200,
+    response: {
+      blacklisted: false
+    }
+  });
+
+  await authme.run(message, { username: saUsername });
+
+  expect(message.say).toHaveBeenCalledWith("`!authme` doesn't appear to be set up properly here. Please contact a guild admin and ask them to \"re-activate auth with an updated role ID\".");
+});
+
+test('logs message when no log channel is specified for auth success message', async () => {
+  // Guild is enrolled in GDN but has no log channel ID
+  moxios.stubRequest(GDN_GUILD, {
+    status: 200,
+    response: {
+      validated_role_id: roleID,
+      logging_channel_id: null
+    }
+  });
+
+  // Member has authed before
+  moxios.stubRequest(GDN_MEMBER, {
+    status: 200,
+    response: {
+      sa_id: saID
+    }
+  });
+
+  // SA ID is blacklisted
+  moxios.stubRequest(GDN_SA, {
+    status: 200,
+    response: {
+      blacklisted: false
+    }
+  });
+
+  await authme.run(message, { username: saUsername });
+
+  expect(logger.info).toHaveBeenCalledWith({ req_id: message.id }, 'No channel ID provided');
+});
+
+test('logs message when an invalid log channel is specified for auth success message', async () => {
+  // Guild is enrolled in GDN but has no log channel ID
+  moxios.stubRequest(GDN_GUILD, {
+    status: 200,
+    response: {
+      validated_role_id: roleID,
+      logging_channel_id: 'badChannelId'
+    }
+  });
+
+  // Member has authed before
+  moxios.stubRequest(GDN_MEMBER, {
+    status: 200,
+    response: {
+      sa_id: saID
+    }
+  });
+
+  // SA ID is blacklisted
+  moxios.stubRequest(GDN_SA, {
+    status: 200,
+    response: {
+      blacklisted: false
+    }
+  });
+
+  await authme.run(message, { username: saUsername });
+
+  expect(logger.info).toHaveBeenCalledWith({ req_id: message.id }, 'No channel found by that ID');
 });
