f2a0100e4c890d7257e60fb4c55c085cc4ade9e1
Matthew Miller <matthew@millerti.me>
2019-04-11 17:52:58 -0700

    Fix logger mock getLogTag implementation

--- src/helpers/__mocks__/logger.js
+++ src/helpers/__mocks__/logger.js
@@ -2,7 +2,7 @@ const loggerMock = {
   info: jest.fn(),
   warn: jest.fn(),
   error: jest.fn(),
-  getLogTag: id => ({ id })
+  getLogTag: id => ({ req_id: id })
 };
 
 module.exports = loggerMock;
