2e360e31a5a30ae7275db59c6eda5ee67bef9e00
Matthew Miller <matthew@millerti.me>
2019-04-21 23:51:39 -0700

    Refactor getSAID to take in profile
    This now takes in the profile instance returned by getSAProfile()

--- src/helpers/auth/getSAID.js
+++ src/helpers/auth/getSAID.js
@@ -1,17 +1,10 @@
-const cheerio = require('cheerio');
 const { oneLine } = require('common-tags');
 
 const logger = require('../logger');
-const { axiosSA, SA_URLS } = require('../axiosSA');
 
 const reasonNoIDFound = oneLine`
   I could not find an ID on the SA profile page for the username you provided. The bot owner has
   been notified. Thank you for your patience while they get this fixed!
-`;
-
-const reasonCatchError = oneLine`
-  A system error occurred while attempting to retrieve your account ID. The bot owner has been
-  notified. Thank you for your patience while they get this fixed!
 `;
 
 /**
@@ -21,48 +14,36 @@ const reasonCatchError = oneLine`
  * assuming the username is valid.
  *
  * @param {object} tag - The output from a call to logger.getLogTag()
- * @param {string} username - The member's SA Username
+ * @param {CheerioElement} profile - The user's profile page HTML wrapped in Cheerio
  * @return {object} - { id }
  */
-async function getSAID ({ tag, username }) {
+async function getSAID ({ tag, profile }) {
   logger.info(tag, 'Retrieving SA ID from SA profile');
 
-  const url = `${SA_URLS.PROFILE}${encodeURIComponent(username)}`;
+  // Prepare to parse it
+  const $ = profile;
+  // Try to grab the ID
+  const id = $('input[name="userid"]').val();
 
-  try {
-    // Request HTML
-    const resp = await axiosSA.get(url);
-    // Prepare to parse it
-    const $ = cheerio.load(resp.data);
-    // Try to grab the ID
-    const id = $('input[name="userid"]').val();
-
-    /**
-     * Note to self: If no ID is returned, then perhaps:
-     *
-     * 1) the cookies above are invalid (maybe the account is banned)
-     * 2) Something about the profile page HTML changed
-     * 3) Maybe the username WAS mis-spelled
-     */
-    if (!id) {
-      logger.error(tag, `No user ID was found on ${url}`);
-      return {
-        id: null,
-        reason: reasonNoIDFound
-      };
-    }
-
-    logger.info(tag, `Found SA ID: ${id}`);
-    return {
-      id
-    };
-  } catch (err) {
-    logger.error({ ...tag, err }, 'System error retrieving SA user ID');
+  /**
+   * Note to self: If no ID is returned, then perhaps:
+   *
+   * 1) the cookies above are invalid (maybe the account is banned)
+   * 2) Something about the profile page HTML changed
+   * 3) Maybe the username WAS mis-spelled
+   */
+  if (!id) {
+    logger.error(tag, `No user ID was found`);
     return {
       id: null,
-      reason: reasonCatchError
+      reason: reasonNoIDFound
     };
   }
+
+  logger.info(tag, `Found SA ID: ${id}`);
+  return {
+    id
+  };
 }
 
 module.exports = getSAID;
