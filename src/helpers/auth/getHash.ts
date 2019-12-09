522405b942be3219120c00044780d5b2dab57da3
Matthew Miller <matthew@millerti.me>
2019-12-09 14:18:58 -0800

    Update getHash to .ts

--- src/helpers/auth/getHash.ts
+++ src/helpers/auth/getHash.ts
@@ -1,7 +1,13 @@
-const { oneLine } = require('common-tags');
+import { oneLine } from 'common-tags';
 
-const { axiosGoonAuth, GOON_AUTH_URLS } = require('../axiosGoonAuth');
-const logger = require('../logger');
+import { axiosGoonAuth, GOON_AUTH_URLS } from '../axiosGoonAuth';
+import logger, { LogTag } from '../logger';
+import { GuildMember } from 'discord.js';
+
+interface Hash {
+  hash?: string;
+  reason?: string;
+}
 
 const errorNoHash = oneLine`
   A system error occurred while generating a hash to help you to verify your SA membership.
@@ -10,26 +16,19 @@ const errorNoHash = oneLine`
 
 /**
  * Get a hash from GoonAuth for the user to place in their SA profile
- *
- * @param {object} tag - The output from a call to logger.getLogTag()
- * @param {Member} member - The Discord member attempting to auth
- * @param {string} username - The Discord member's SA username
- * @returns {object} - { hash, reason? }
  */
-const getHash = async ({ tag, member, username }) => {
+export default async function getHash (tag: LogTag, member: GuildMember, username: string): Promise<Hash> {
   try {
     logger.info(tag, `Requesting hash for SA user: ${username}`);
     const { data } = await axiosGoonAuth.post(GOON_AUTH_URLS.GET_HASH, { username });
     return {
-      hash: data.hash
+      hash: data.hash,
     };
   } catch (err) {
     logger.error({ ...tag, err }, 'Error requesting hash');
     return {
       hash: null,
-      reason: errorNoHash
+      reason: errorNoHash,
     };
   }
-};
-
-module.exports = getHash;
+}
