2718500c35870aed2f20213262c8a803e776a0d1
Matthew Miller <matthew@millerti.me>
2019-12-09 14:03:55 -0800

    Refactor axiosSA to .ts

--- src/helpers/axiosSA.ts
+++ src/helpers/axiosSA.ts
@@ -1,27 +1,22 @@
-const axios = require('axios');
+import axios from 'axios';
 
 const COOKIES = {
   sessionid: process.env.SESSION_ID,
   sessionhash: process.env.SESSION_HASH,
   bbuserid: process.env.BBUSER_ID,
-  bbpassword: process.env.BBPASSWORD
+  bbpassword: process.env.BBPASSWORD,
 };
 
 // Format the above cookies into a value suitable for use as the Cookie header
 const Cookie = Object.entries(COOKIES).map(([key, val]) => `${key}=${val}`).join('; ');
 
 // Prepare an Axios client with SA user cookies
-const axiosSA = axios.create({
+export const axiosSA = axios.create({
   headers: {
-    Cookie
-  }
+    Cookie,
+  },
 });
 
-const SA_URLS = {
-  PROFILE: 'http://forums.somethingawful.com/member.php?action=getinfo&username='
-};
-
-module.exports = {
-  axiosSA,
-  SA_URLS
+export const SA_URLS = {
+  PROFILE: 'http://forums.somethingawful.com/member.php?action=getinfo&username=',
 };
