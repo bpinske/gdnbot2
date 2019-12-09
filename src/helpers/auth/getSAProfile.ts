ccd3ac3f857db6cc0a283b9c9136dee105761704
Matthew Miller <matthew@millerti.me>
2019-12-09 14:03:26 -0800

    Refactor getSAProfile to .ts

--- src/helpers/auth/getSAProfile.ts
+++ src/helpers/auth/getSAProfile.ts
@@ -1,8 +1,13 @@
-const cheerio = require('cheerio');
-const { oneLine } = require('common-tags');
+import cheerio from 'cheerio';
+import { oneLine } from 'common-tags';
 
-const logger = require('../logger');
-const { axiosSA, SA_URLS } = require('../axiosSA');
+import logger, { LogTag } from '../logger';
+import { axiosSA, SA_URLS } from '../axiosSA';
+
+interface SAProfile {
+  reason?: string;
+  profile?: CheerioStatic,
+}
 
 const reasonErrorLoadingProfile = oneLine`
   A system error occurred while reading your SA profile. The bot owner has been notified. Thank
@@ -16,27 +21,24 @@ const reasonErrorLoadingProfile = oneLine`
  * @param {string} username - The member's SA Username
  * @returns {CheerioElement} - { profile, reason? }
  */
-const getSAProfile = async ({ tag, username }) => {
+export default async function getSAProfile (tag: LogTag, username: string): Promise<SAProfile> {
   logger.info(tag, 'Retrieving SA profile page');
 
   const url = `${SA_URLS.PROFILE}${encodeURIComponent(username)}`;
 
   try {
     // Request HTML
-    const resp = await axiosSA.get(url);
+    const resp = await axiosSA.get<string>(url);
     // Wrap it in Cheerio for easy traversal
     const profile = cheerio.load(resp.data);
 
     return {
-      profile
+      profile,
     };
   } catch (err) {
     logger.error({ ...tag, err }, 'Error retrieving SA profile page');
     return {
-      profile: null,
-      reason: reasonErrorLoadingProfile
+      reason: reasonErrorLoadingProfile,
     };
   }
-};
-
-module.exports = getSAProfile;
+}
