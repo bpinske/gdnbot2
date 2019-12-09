f91012357ec60947867dc0d52c13949590b56b90
Matthew Miller <matthew@millerti.me>
2019-12-09 11:12:04 -0800

    Convert axiosGDN to .ts

--- src/helpers/axiosGDN.ts
+++ src/helpers/axiosGDN.ts
@@ -1,24 +1,19 @@
-const axios = require('axios');
+import axios from 'axios';
 
 /**
  * An instance of Axios configured to speak with the GDN APIs over Docker's internal network
  */
-const axiosGDN = axios.create({
+export const axiosGDN = axios.create({
   baseURL: 'http://gdn',
   headers: {
-    'Authorization': `Token ${process.env.GDN_API_TOKEN}`,
+    Authorization: `Token ${process.env.GDN_API_TOKEN}`,
     'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  }
+    Accept: 'application/json',
+  },
 });
 
-const GDN_URLS = {
+export const GDN_URLS = {
   MEMBERS: '/gdn/members',
   GUILDS: '/gdn/servers',
-  SA: '/gdn/sa'
-};
-
-module.exports = {
-  axiosGDN,
-  GDN_URLS
+  SA: '/gdn/sa',
 };
