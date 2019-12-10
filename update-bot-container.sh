478a7b36cbeef6b62286c1717436a0b59075f886
Matthew Miller <matthew@millerti.me>
2019-12-10 12:01:17 -0800

    Remove reference to nginx in update bot script

--- update-bot-container.sh
+++ update-bot-container.sh
@@ -7,5 +7,5 @@ echo "[---removing containers---]"
 docker-compose rm -f
 echo "[---rebuilding bot---]"
 docker-compose build bot
-echo "[---restarting bot and nginx---]"
+echo "[---restarting bot---]"
 docker-compose -f docker-compose.yml up -d --no-deps bot
