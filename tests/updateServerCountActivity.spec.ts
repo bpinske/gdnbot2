1308369c46a41c77874519d11c7ff654dcc4abd1
Matthew Miller <matthew@millerti.me>
2019-12-09 20:25:39 -0800

    Fix updateServerCountActivity unittest

--- tests/updateServerCountActivity.spec.ts
+++ tests/updateServerCountActivity.spec.ts
@@ -1,25 +1,27 @@
-const updateServerCountActivity = require('../src/eventHandlers/updateServerCountActivity');
+import { CommandoClient } from 'discord.js-commando';
+
+import updateServerCountActivity from '../src/eventHandlers/updateServerCountActivity';
 
 // jest.unmock('../src/helpers/logger');
-const logger = require('../src/helpers/logger');
+import logger from '../src/helpers/logger';
 
 const bot = {
   guilds: {
-    size: 4
+    size: 4,
   },
   user: {
-    setActivity: jest.fn()
-  }
-};
+    setActivity: jest.fn(),
+  },
+} as unknown as CommandoClient;
 
 test('set bot activity to reflect current number of joined guilds', async () => {
-  await updateServerCountActivity({ bot });
+  await updateServerCountActivity(bot);
 
   expect(bot.user.setActivity).toHaveBeenCalledWith(`in ${bot.guilds.size} servers`);
 });
 
 test('logs updated activity string', async () => {
-  await updateServerCountActivity({ bot });
+  await updateServerCountActivity(bot);
 
   expect(logger.info).toHaveBeenCalledWith(`setting activity to "in ${bot.guilds.size} servers"`);
 });
