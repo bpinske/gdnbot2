300033f3b1d1d5cf6dcdabf5f31ac5f4f01a54f7
Matthew Miller <matthew@millerti.me>
2019-12-11 12:11:52 -0800

    Finish mapping out enrollment flow
    Includes a few TODO’s

--- src/commands/gdn/enrollServer.ts
+++ src/commands/gdn/enrollServer.ts
@@ -1,15 +1,24 @@
-import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
-import { stripIndents } from 'common-tags';
+import { Command, CommandoClient, CommandoMessage, ArgumentCollector } from 'discord.js-commando';
+import { stripIndents, oneLine } from 'common-tags';
+
+import logger, { getLogTag } from '../../helpers/logger';
+import { API_ERROR } from '../../helpers/constants';
+import roundDown from '../../helpers/roundDown';
 
 interface EnrollCommandArgs {
   description: string;
-  inviteURL: string;
+  inviteCode: string;
+}
+
+interface ConfirmArg {
+  confirm: boolean;
 }
 
 export default class ListCommand extends Command {
   constructor (client: CommandoClient) {
     super(client, {
       name: 'gdn_enroll_server',
+      aliases: ['gdn_enroll'],
       group: 'gdn',
       memberName: 'enroll_server',
       description: 'Enroll this server in Goon Discord Network',
@@ -18,25 +27,142 @@ export default class ListCommand extends Command {
       args: [
         {
           key: 'description',
-          prompt: 'Enter a short description for this server (limit 300 chars):',
+          prompt: 'enter a short description for this server (limit 300 chars):',
           type: 'string',
-          default: '',
+          wait: 60,
         },
         {
-          key: 'inviteURL',
-          prompt: 'enter an Instant Invite URL for this server:',
+          key: 'inviteCode',
+          prompt: 'enter a **non-expiring** Invite Code for this server:',
           type: 'string',
-          default: '',
+          wait: 60,
         },
       ],
     });
   }
 
-  run (message: CommandoMessage, { description, inviteURL }: EnrollCommandArgs) {
-    return message.say(stripIndents`
-      Description: ${description}
+  async run (message: CommandoMessage, { description, inviteCode }: EnrollCommandArgs) {
+    const {
+      id,
+      name,
+      memberCount,
+      iconURL,
+    } = message.guild;
 
-      Invite URL: ${inviteURL}
+    const tag = getLogTag(message.id);
+
+    logger.info(tag, `[EVENT START: !${this.name}]`);
+
+    logger.debug(tag, `Called in ${name} (${id})`);
+
+    // Give some feedback that the bot is doing something
+    message.channel.startTyping();
+
+    logger.info(tag, 'Checking to see if the server is already enrolled');
+    // TODO
+    // checks/hasGuildEnrolled()
+
+    logger.info(tag, 'Checking to see if the user has authed and can interact with GDN');
+    // TODO
+    // checks/hasMemberAuthed()
+    // checks/isMemberBlacklisted()
+
+    logger.info(tag, `Confirming invite code "${inviteCode}" is valid and won't expire`);
+
+    // See if the provided invite code is for an invite that'll expire
+    let invite;
+    try {
+      invite = await this.client.fetchInvite(inviteCode);
+    } catch (err) {
+      if (err.code === API_ERROR.UNKNOWN_INVITE) {
+        logger.info(tag, 'Invalid invite code, exiting');
+
+        return message.reply(stripIndents`
+          "${inviteCode}" doesn't seem to be a valid invite code for this guild.
+
+          ${oneLine`
+            Please double-check your invites in **Server Settings > Invites** and then try
+            ${this.usage()} again with a **non-expiring** invite code.
+          `}
+        `);
+      }
+
+      logger.error({ ...tag, err }, 'Error retrieving invite');
+      throw err;
+    }
+
+    /**
+     * TODO: `invite.expiresAt` via `this.client.fetchInvite()` will _always_ be null. To fix this,
+     * the bot would need to be granted the MANAGE_GUILD/"Manage Server" permission, at which point
+     * we could use message.guild.fetchInvites(), .get() by `inviteCode`, and view that info...
+     */
+    // null = eternal, datetime = expires
+    if (invite.expiresAt !== null) {
+      logger.info(tag, 'User specified an expiring invite, exiting');
+
+      return message.reply(stripIndents`
+        The specified server invite will eventually expire, after which it will become impossible
+        for users to join this server from the GDN homepage.
+
+        ${oneLine`
+          Please double-check your invites in **Server Settings > Invites** and then try
+          ${this.usage()} again with a **non-expiring** invite code.
+        `}
+      `);
+    }
+
+    logger.info(tag, 'Invite code is valid for enrollment');
+
+    // Prepare server details for submission
+    const details = {
+      name,
+      server_id: id,
+      description: description.substr(0, 300),
+      icon_url: iconURL,
+      user_count: roundDown(memberCount),
+      invite_url: `https://discord.gg/${inviteCode}`,
+    };
+
+    const confirmCollector = new ArgumentCollector(this.client, [
+      {
+        key: 'confirm',
+        prompt: stripIndents`
+          does everything look correct?
+
+          **Server Name:** ${details.name}
+          **Description:** ${details.description}
+          **Num. Users:** ${details.user_count}
+          **Invite URL:** ${details.invite_url}
+
+        `,
+        type: 'boolean',
+      },
+    ], 60);
+
+    // Wait for the user to confirm details
+    message.channel.stopTyping();
+    const resp = await confirmCollector.obtain(message);
+
+    const values = (resp.values as ConfirmArg);
+
+    if (!values?.confirm) {
+      logger.info(tag, 'User did not confirm that values correct, exiting');
+      return message.reply('the server was not enrolled. Feel free to try again, though!');
+    }
+
+    message.channel.startTyping();
+
+    logger.info({ ...tag, details }, 'Submitting server to GDN API');
+    // TODO
+    // helpers/axiosGDN.post(GDN_URLS.GUILDS, details);
+
+    message.channel.stopTyping();
+
+    logger.info(tag, '[EVENT STOP]');
+
+    return message.reply(oneLine`
+      welcome aboard! This server has been enrolled in GDN and is now discoverable at
+      https://goondiscordnetwork.com :bee:
     `);
   }
 }
