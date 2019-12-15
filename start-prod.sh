f6c164262b7db9ad1bebb4b363f63b4962502894
Matthew Miller <matthew@millerti.me>
2019-12-15 12:07:47 -0800

    Replace update-bot-container.sh with start-prod.sh

--- start-prod.sh
+++ start-prod.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker-compose -f docker-compose.yml up -d
+echo "[---pulling latest code---]"
+git pull
+echo "[---stopping prod bot---]"
+docker-compose stop bot
+echo "[---removing containers---]"
+docker-compose rm -f
+echo "[---restarting prod bot---]"
+docker-compose -f docker-compose.yml up -d --no-deps bot
--- update-bot-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "[---pulling latest code---]"
-git pull
-echo "[---stopping bot---]"
-docker-compose stop bot
-echo "[---removing containers---]"
-docker-compose rm -f
-echo "[---restarting bot---]"
-docker-compose -f docker-compose.yml up -d --no-deps bot
