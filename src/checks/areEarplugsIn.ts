208c1d70cdf4c072228ac7fe7ead5db6617b6fd2
Matthew Miller <matthew@millerti.me>
2019-12-15 18:46:56 -0800

    Rename check to areEarplugsIn

--- src/checks/areEarplugsIn.ts
+++ src/checks/areEarplugsIn.ts
@@ -2,7 +2,7 @@ import { GuildSettingsHelper } from 'discord.js-commando';
 
 import { SETTINGS } from '../helpers/constants';
 
-export default async function checkEarplugsIn (settings: GuildSettingsHelper): Promise<boolean> {
+export default async function areEarplugsIn (settings: GuildSettingsHelper): Promise<boolean> {
   const earplugsIn = await settings.get(SETTINGS.EARPLUGS_IN, false);
   return Boolean(earplugsIn);
 }
