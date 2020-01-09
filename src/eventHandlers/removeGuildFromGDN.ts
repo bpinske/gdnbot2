8c3e76b8073438af8a7cf63f4bbe4d9eaeba1717
Matthew Miller <matthew@millerti.me>
2020-01-08 18:13:58 -0800

    Add helper to delete guild from GDN

---
+++ src/eventHandlers/removeGuildFromGDN.ts
@@ -0,0 +1,21 @@
+import { Guild } from 'discord.js';
+
+import logger, { LogTag } from '../helpers/logger';
+import { axiosGDN, GDN_URLS } from '../helpers/axiosGDN';
+import { AxiosError } from 'axios';
+
+/**
+ * Remove a Discord Guild from GDN
+ */
+export default function removeGuildFromGDN (tag: LogTag, guild: Guild) {
+  return axiosGDN.delete(`${GDN_URLS.GUILDS}/${guild.id}`)
+    .then(() => {
+      logger.info(tag, `Deleted guild ${guild.name} (${guild.id}) from GDN`);
+    })
+    .catch((err: AxiosError) => {
+      // 404 means we tried to delete a server not enrolled in GDN
+      if (err.response?.status !== 404) {
+        logger.error({ ...tag, err }, `Error deleting guild ${guild.name} (${guild.id}) from GDN`);
+      }
+    });
+}
